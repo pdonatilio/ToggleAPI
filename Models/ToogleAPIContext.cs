5773cd0eeee16a9ae4eb4e17fe8f349f89a7bdbe
pdonatilio <pdonatilio@gmail.com>
2018-08-26 17:58:53 -0300

    First Commit

---
+++ Models/ToogleAPIContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ToggleAPI.Models
+{
+    public class ToogleAPIContext : DbContext
+    {
+        public ToogleAPIContext(DbContextOptions<ToogleAPIContext> options) : base(options) {}
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using ToggleAPI.Models;
+
+namespace ToggleAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            //MySql connection
+            services.AddDbContextPool<ToogleAPIContext>(
+                options => options.UseMySql(Configuration.GetConnectionString("MysqlConnection"),
+                    mySqlOptionsAction =>
+                    {
+                        mySqlOptionsAction.ServerVersion(new Version(8, 0, 12), ServerType.MySql);
+                    }
+
+                )
+            );
+
+            //Redis connection
+            services.AddDistributedRedisCache(
+                options =>
+                {
+                    options.Configuration = "localhost";
+                    options.InstanceName = "redis_test";
+                }
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
