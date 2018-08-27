3aa1d78b8b53c05ef38a5cf7c75ee825d25ffb32
pdonatilio <pdonatilio@gmail.com>
2018-08-27 11:28:33 -0300

    Including the User Model, Dto and migrations structures

--- Models/ToogleAPIContext.cs
+++ Models/ToogleAPIContext.cs
@@ -5,6 +5,8 @@ namespace ToggleAPI.Models
     public class ToogleAPIContext : DbContext
     {
         public ToogleAPIContext(DbContextOptions<ToogleAPIContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
         
     }
 }
