76d01c5779c7de6a1ef9cea44bc585a7f9f30cf0
pdonatilio <pdonatilio@gmail.com>
2018-08-28 13:58:34 -0300

    Including Swagger to the Project

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -22,8 +22,15 @@ namespace ToggleAPI.Controllers
             _userService = userService;
         }
 
-        // GET api/User
+        /// <summary>
+        /// Get all users.
+        /// </summary>
+        /// <returns>A list with all users</returns>
+        /// <response code="200">Sucess</response>
+        /// <response code="401">Unauthorized Access</response> 
         [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
         public IActionResult Get()
         {
             try
@@ -38,8 +45,24 @@ namespace ToggleAPI.Controllers
             
         }
 
-        // GET api/User/{id}
+        /// <summary>
+        /// Get a specific user by id.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET api/User/{id}
+        ///
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <returns>A specific user</returns>
+        /// <response code="200">Returns the user informed by Id</response>
+        /// <response code="400">If id is null or not found</response>
+        /// <response code="401">Unauthorized Access</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public IActionResult Get(int id)
         {
             try
@@ -54,9 +77,28 @@ namespace ToggleAPI.Controllers
             
         }
 
-        // POST api/User
+        /// <summary>
+        /// Create a New user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST api/User/
+        ///     
+        ///     {
+        ///         "firstName": "string",
+        ///         "lastName": "string",
+        ///         "username": "string",
+        ///         "password": "string",
+        ///     }        
+        /// </remarks>
+        /// <returns>OK</returns>
+        /// <response code="200">Sucess</response>
+        /// <response code="400">If username exists or password is null</response>
         [AllowAnonymous]
         [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public IActionResult Post([FromBody]UserDto userDto)
         {
             try
@@ -70,11 +112,28 @@ namespace ToggleAPI.Controllers
             }
         }
 
-        // POST api/User/auth
         // User authentication passing name and password by json file
+        /// <summary>
+        /// Get the access token to use the API
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT api/User/auth
+        ///     
+        ///     {
+        ///         "username": "string",
+        ///         "password": "string",
+        ///     }        
+        /// </remarks>
+        /// <returns>The user data and the security token</returns>
+        /// <response code="200">Returns the user data and security token</response>
+        /// <response code="400">If id is null or not found or the username is invalid</response>
         [AllowAnonymous]
-        [HttpPost("{auth}")]
-        public IActionResult Authenticate([FromBody]UserDto userDto)
+        [HttpPost("auth")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult Auth([FromBody]UserDto userDto)
         {
             try
             {
@@ -85,8 +144,31 @@ namespace ToggleAPI.Controllers
                 return BadRequest(new { message = ex.Message });
             }
         }
-        // PUT api/User/{id}
+
+        /// <summary>
+        /// Update the user informed by Id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST api/User/auth
+        ///     
+        ///     {
+        ///         "firstName": "string",
+        ///         "lastName": "string",
+        ///         "username": "string",
+        ///         "password": "string",
+        ///     }
+        ///
+        /// The fiedls username and password are optionals
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <returns>OK</returns>
+        /// <response code="200">Sucess</response>
+        /// <response code="400">If the id is null or not found or the username is invalid</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]        
         public IActionResult Put(int id, [FromBody]UserDto userDto)
         {
             try
@@ -101,6 +183,20 @@ namespace ToggleAPI.Controllers
         }
 
         // DELETE api/User/{id}
+        /// <summary>
+        /// Delet a specific user by id.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     DEL api/User/{id}
+        ///
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <returns>OK</returns>
+        /// <response code="200">Sucess</response>
+        /// <response code="400">If id is null or not found</response>
+        /// <response code="401">Unauthorized Access</response>
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +14,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using AutoMapper;
+using Swashbuckle.AspNetCore.Swagger;
 using ToggleAPI.Models;
 using ToggleAPI.Services;
 using ToggleAPI.Dtos;
@@ -97,6 +101,48 @@ namespace ToggleAPI
                     ValidateAudience = false
                 };
             });
+
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info 
+                { 
+                    Title = "Toggle API", 
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core 2.1 Web API",
+                    Contact = new Contact
+                    {
+                        Name = "Paulo Donatilio",
+                        Email = "pdonatilio@gmail.com",
+                        Url = "https://www.linkedin.com/in/pdonatilio/"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under Gnu GPL",
+                        Url = "https://www.gnu.org/licenses/gpl-3.0.pt-br.html"
+                    } 
+                });
+                c.DescribeAllEnumsAsStrings();
+                // JWT-token Swagger authentication by password
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(security);
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -112,6 +158,16 @@ namespace ToggleAPI
             }
            
             app.UseAuthentication();
+            app.UseSwagger();
+
+            // Confirgure the swagger user interface
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Toggle API v1");
+                
+                c.DocumentTitle = "Title Documentation";
+                c.DocExpansion(DocExpansion.None);
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
